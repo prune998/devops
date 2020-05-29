33b4a0c64c988d011dc83ad8c968558e24550bd5
prune <prune@lecentre.net>
2020-05-29 09:38:14 -0400

    added go mod and error code return for droppingserver

---
+++ WEB/droppingServer/go.mod
@@ -0,0 +1,3 @@
+module github.com/prune998/devops/WEB/droppingServer
+
+go 1.14
--- WEB/droppingServer/main.go
+++ WEB/droppingServer/main.go
@@ -7,19 +7,24 @@ import (
 	"net/http"
 	"strconv"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", defaultHandler)
-	http.HandleFunc("/hj", hjHandler)
-	http.HandleFunc("/world", helloWorldHandler)
-	http.HandleFunc("/slow", slowHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", defaultHandler)
+	r.HandleFunc("/hj", hjHandler)
+	r.HandleFunc("/world", helloWorldHandler)
+	r.HandleFunc("/slow", slowHandler)
+	r.HandleFunc("/status/{code}", statusHandler)
 
 	fmt.Println("Listening on port 8443 for TLS")
-	go http.ListenAndServeTLS(":8443", "server.crt", "server.key", nil)
+	go http.ListenAndServeTLS(":8443", "server.crt", "server.key", r)
 
 	fmt.Println("Listening on port 8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", r)
 
 }
 
@@ -73,4 +78,35 @@ func slowHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	time.Sleep(time.Duration(sleepDelay) * time.Second)
 	io.WriteString(w, "Hello world!")
+}
+
+// slowHandler return a "Hello World" message after a delay, defaults to 20s
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	i, err := strconv.Atoi(vars["code"])
+	if err != nil {
+		http.Error(w, "error code not supported", http.StatusInternalServerError)
+		return
+	}
+
+	// sleep if needed
+	delay, ok := r.URL.Query()["delay"]
+
+	if !ok || len(delay[0]) < 1 {
+		log.Println("Url Param 'delay' is missing from the /slow call. Use /slow?delay=20")
+		return
+	}
+
+	// convert string delay to int
+	sleepDelay, err := strconv.Atoi(delay[0])
+	if err != nil {
+		log.Println("delay parameter not set, using 20s")
+		sleepDelay = 20
+	}
+	time.Sleep(time.Duration(sleepDelay) * time.Second)
+
+	w.WriteHeader(i)
+	fmt.Fprintf(w, "Error: %d\n", i)
 }
