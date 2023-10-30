e353a9078706e685b119e5b6cb539a2455b40716
Sebastien THOMAS <sebastien.thomas@wunderkind.co>
2023-10-30 09:43:50 -0400

    rework CPU Consumer

--- K8S/CPUconsumer/main.go
+++ K8S/CPUconsumer/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
+	"strconv"
 	"syscall"
 	"time"
 
@@ -27,6 +28,8 @@ var (
 	waitDuration   = flag.Duration("waitDuration", time.Duration(30)*time.Second, "how long to wait before work, in Golang Duration")
 	workDuration   = flag.Duration("workDuration", time.Duration(10)*time.Second, "how long to work, then wait, in Golang Duration")
 	httpPort       = flag.String("httpport", "7789", "port to bind for HTTP server used for Health and Metrics")
+	numCPU         = strconv.Itoa(runtime.NumCPU()) // Keep track on the number of CPU as set by GOMAXPROCS
+	numMaxProcs    = strconv.Itoa(runtime.GOMAXPROCS(-1))
 )
 
 func printVersion() {
@@ -75,7 +78,6 @@ func main() {
 			http.Error(w, "no valid point for this device_id", 500)
 			return
 		}
-
 		w.Write(b)
 	})
 
@@ -84,90 +86,78 @@ func main() {
 
 	// listen on the HTTP port for metrics
 	go func() {
-		log.Warn(fmt.Sprintf("listening HTTP (metrics & health) on %v", *httpPort))
+		log.Info(fmt.Sprintf("listening HTTP (metrics & health) on %v", *httpPort))
 		log.Warn(http.ListenAndServe(fmt.Sprintf(":%s", *httpPort), nil))
 	}()
 
+	log.Info(fmt.Sprintf("app started on a %s CPU server with MAXPROCS %s", numCPU, numMaxProcs))
+
 	// values for the working period
 	n, m, t := setCPU(*workCPU)
-	runtime.GOMAXPROCS(t)
+	// runtime.GOMAXPROCS(t) // we don't override the GOMAXPROCS as we set it in the env
 
 	// values for the waiting period
 	a, s, v := setCPU(*waitCPU)
 
-	// quit channel is used to force exit of all threads
-	quit := make(chan bool, t)
+	// main context to cancel when we get a stop signal
+	ctx, cancel := context.WithCancel(context.Background())
 
 	// Grab SIGINT and exit all threads
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func(t int) {
+	go func() {
 		<-c
 		log.Info("got SIGINT, terminating")
-		cleanup(t, quit)
+		cancel()
+		time.Sleep(time.Duration(10) * time.Millisecond)
 		os.Exit(1)
-	}(t)
+	}()
 
 	for {
 		// start all consumer threads
-		log.Info(fmt.Sprintf("starting %d+%.3f (%d total) working threads\n", n, m, t))
-		ctx, cancel := context.WithCancel(context.Background())
-		work(ctx, n, m)
+		log.Info(fmt.Sprintf("starting %d+%.3f (%d total) working threads", n, m, t))
+		ctx1, cancel1 := context.WithCancel(ctx)
+		work(ctx1, n, m, log)
 
-		log.Info(fmt.Sprintf("working for %s\n", *workDuration))
+		log.Info(fmt.Sprintf("working for %s", *workDuration))
 		time.Sleep(*workDuration)
 
-		// for i := 0; i < t; i++ {
-		// 	fmt.Println("sending quit signal for full")
-		// 	quit <- true
-		// 	time.Sleep(time.Duration(10) * time.Millisecond)
-		// }
-		cancel()
-		log.Info(fmt.Sprintf("Job done, starting %d+%.3f (%d total) waiting threads for %s\n", a, s, v, *waitDuration))
+		cancel1()
+		log.Info(fmt.Sprintf("Job done, starting %d+%.3f (%d total) waiting threads for %s", a, s, v, *waitDuration))
 
-		if *waitCPU > 0 {
-			ctx, cancel := context.WithCancel(context.Background())
-			work(ctx, a, s)
+		if v > 0 {
+			ctx2, cancel2 := context.WithCancel(ctx)
+			work(ctx2, a, s, log)
 
+			log.Info(fmt.Sprintf("working for %s", *waitDuration))
 			time.Sleep(*waitDuration)
+			cancel2()
 
-			// for i := 0; i < v; i++ {
-			// 	fmt.Println("sending quit signal for fraction")
-			// 	quit <- true
-			// 	time.Sleep(time.Duration(10) * time.Millisecond)
-			// }
-			cancel()
 		} else {
 			time.Sleep(*waitDuration)
 		}
 	}
 }
 
-// cleanup terminate the program clean
-func cleanup(t int, quit chan<- bool) {
-	fmt.Printf("cleaning up %d threads\n", t)
-	for i := 0; i < t; i++ {
-		quit <- true
-		time.Sleep(time.Duration(10) * time.Millisecond)
-	}
-}
-
 // work will start some routines to consume CPU
-func work(ctx context.Context, n int, m float64) {
+func work(ctx context.Context, n int, m float64, log *logrus.Entry) {
+	callingTime := time.Now().UnixNano()
+
 	// start a goroutine that will not consume 100% of a CPU
 	if m > 0 {
 		PromThreadGauge.Inc()
-		go func() {
-			fmt.Println("fraction thread started")
+		go func(jobID int64) {
+			log.Info(fmt.Sprintf("fraction thread started (%d)", jobID))
 			dummy := 0
 			for {
 				select {
 				case <-ctx.Done():
 					PromThreadGauge.Dec()
-					fmt.Println("fraction thread stopped")
+					log.Info(fmt.Sprintf("fraction thread stopped (%d)", jobID))
 					return
 				default:
 					start := time.Now().UnixNano() / int64(time.Millisecond)
+					// This for loop represent the work to be done in the % of the CPU allocated
 					for {
 						end := time.Now().UnixNano() / int64(time.Millisecond)
 						if end-start > int64(m*100) {
@@ -177,37 +167,79 @@ func work(ctx context.Context, n int, m float64) {
 							time.Sleep(time.Duration(b) * time.Millisecond)
 							break
 						}
+						PromOpsCounter.WithLabelValues(numMaxProcs).Inc()
+						dummy++
+						if dummy%1000000000 == 0 {
+							log.Info(fmt.Sprintf("fraction thread still working (%d)", jobID))
+							dummy = 1
+						}
 					}
-					dummy++
-
-					// end := time.Now().UnixNano() / int64(time.Millisecond)
-					// diff := end - start
-					// log.Printf("Duration(ms): %d", diff)
 				}
 			}
-		}()
+		}(callingTime + int64(n))
 	}
 	// start routines that will consume all the full CPUs requested
+	// for i := 0; i < n; i++ {
+	// 	PromThreadGauge.Inc()
+	// 	go func(jobID int64) {
+	// 		log.Info(fmt.Sprintf("full thread started (%d)", jobID))
+	// 		dummy := 0
+	// 		for {
+	// 			select {
+	// 			case <-ctx.Done():
+	// 				PromThreadGauge.Dec()
+	// 				log.Info(fmt.Sprintf("full thread stopped (%d)", jobID))
+	// 				return
+	// 			default:
+	// 				PromOpsCounter.WithLabelValues(numMaxProcs).Inc()
+	// 				dummy++
+	// 				if dummy%10000000000 == 0 {
+	// 					log.Info(fmt.Sprintf("full thread still working (%d)", jobID))
+	// 					dummy = 1
+	// 				}
+	// 			}
+	// 		}
+	// 	}(callingTime + int64(i))
+	// }
+
+	// start a full thread but still cap the usage to 100ms
 	for i := 0; i < n; i++ {
 		PromThreadGauge.Inc()
-		go func() {
-			fmt.Println("full thread started")
+		go func(jobID int64) {
+			log.Info(fmt.Sprintf("full thread started (%d)", jobID))
 			dummy := 0
 			for {
 				select {
 				case <-ctx.Done():
 					PromThreadGauge.Dec()
-					fmt.Println("full thread stopped")
+					log.Info(fmt.Sprintf("full thread stopped (%d)", jobID))
 					return
 				default:
-					dummy++
+					start := time.Now().UnixNano() / int64(time.Millisecond)
+					// This for loop represent the work to be done in the % of the CPU allocated
+					for {
+						end := time.Now().UnixNano() / int64(time.Millisecond)
+						if end-start > int64(100) {
+							// sleep few ms to compensate for over-usage
+							b := 100 - end - start
+
+							time.Sleep(time.Duration(b) * time.Millisecond)
+							break
+						}
+						PromOpsCounter.WithLabelValues(numMaxProcs).Inc()
+						dummy++
+						if dummy%1000000000 == 0 {
+							log.Info(fmt.Sprintf("full thread still working (%d)", jobID))
+							dummy = 1
+						}
+					}
 				}
 			}
-		}()
+		}(callingTime + int64(i))
 	}
 }
 
-// setCPU split the number of milicores into full cores + subsides
+// setCPU split the number of milicores into full cores + fraction
 func setCPU(c float64) (n int, m float64, t int) {
 	// Set the number of full CPU
 	n = int(c)
@@ -222,9 +254,10 @@ func setCPU(c float64) (n int, m float64, t int) {
 		n = runtime.NumCPU()
 		t = n
 	} else if m == 0 {
-		// if CPU is round, set GOMAXPROCS to the number of full CPU
+		// if CPU is round, set total number of routines to the number of full CPU
 		t = n
 	} else {
+		// if CPU has a fraction, set total number of routines to the number of full CPU + 1
 		t = n + 1
 	}
 	return n, m, t
--- K8S/CPUconsumer/metrics.go
+++ K8S/CPUconsumer/metrics.go
@@ -7,8 +7,15 @@ var (
 		Name: "cpuconsumer_threads",
 		Help: "Number of running threads",
 	})
+
+	PromOpsCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "cpuconsumer_ops",
+		Help: "Number of operation performed",
+	}, []string{"gomaxprocs"},
+	)
 )
 
 func init() {
 	prometheus.MustRegister(PromThreadGauge)
+	prometheus.MustRegister(PromOpsCounter)
 }
