aee42157ae3b3da6ba963cea7f826bc3d95ef1d0
Prune <prune@lecentre.net>
2024-01-08 09:52:41 -0500

    dump all GCP custom metrics

--- CLOUD/GCP/stackdriver-monitoring/main.go
+++ CLOUD/GCP/stackdriver-monitoring/main.go
@@ -5,7 +5,11 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"runtime"
 	"time"
+
+	"github.com/namsral/flag"
 
 	monitoring "cloud.google.com/go/monitoring/apiv3/v2"
 	"cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
@@ -13,7 +17,32 @@ import (
 	"google.golang.org/api/iterator"
 )
 
+var (
+	version        = "no version set"
+	displayVersion = flag.Bool("version", false, "Show version and quit")
+	logLevel       = flag.String("logLevel", "warn", "log level from debug, info, warning, error. When debug, genetate 100% Tracing")
+	projectID      = flag.String("projectID", "", "Google GCP Project name")
+	metricName     = flag.String("metricName", "storage.googleapis.com/storage/total_bytes", "Full name of the metric to gather")
+)
+
+func printVersion() {
+	fmt.Printf("Go Version: %s\n", runtime.Version())
+	fmt.Printf("Go OS/Arch: %s/%s\n", runtime.GOOS, runtime.GOARCH)
+	fmt.Printf("App Version: %v\n", version)
+}
+
 func main() {
+	// parse flags
+	flag.Parse()
+	if *displayVersion {
+		printVersion()
+		os.Exit(0)
+	}
+
+	if *projectID == "" {
+		fmt.Println("ERROR: You need to set --projectID <GCP PRoject name>")
+		os.Exit(1)
+	}
 	ctx := context.Background()
 
 	// Creates a client.
@@ -23,34 +52,28 @@ func main() {
 	}
 	defer c.Close()
 
-	// Sets your Google Cloud Platform project ID.
-	projectID := "your-google-project-id(name)"
-
-	// Which metric to query
-	metricName := "storage.googleapis.com/storage/total_bytes"
-
 	// list metrics typres you can query for this project
-	// req := &monitoringpb.ListMetricDescriptorsRequest{
-	// 	Name: "projects/" + projectID,
-	// }
-	// iter := c.ListMetricDescriptors(ctx, req)
+	req := &monitoringpb.ListMetricDescriptorsRequest{
+		Name: "projects/" + *projectID,
+	}
+	iter := c.ListMetricDescriptors(ctx, req)
 
-	// for {
-	// 	resp, err := iter.Next()
-	// 	if err == iterator.Done {
-	// 		break
-	// 	}
-	// 	if err != nil {
-	// 		log.Printf("Could not list metrics: %v", err)
-	// 	}
-	// 	fmt.Printf("%v\n", resp.GetType())
-	// }
+	for {
+		resp, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Printf("Could not list metrics: %v", err)
+		}
+		fmt.Printf("%v\n", resp.GetType())
+	}
 
 	// get some infos on the metric
-	fmt.Printf("\nnow requesting metric %s:\n\n", metricName)
+	fmt.Printf("\nnow requesting metric %s:\n\n", *metricName)
 
 	req2 := &monitoringpb.GetMetricDescriptorRequest{
-		Name: fmt.Sprintf("projects/%s/metricDescriptors/%s", projectID, metricName),
+		Name: fmt.Sprintf("projects/%s/metricDescriptors/%s", *projectID, *metricName),
 	}
 	resp2, err := c.GetMetricDescriptor(ctx, req2)
 	if err != nil {
@@ -74,8 +97,8 @@ func main() {
 	startTime := time.Now().UTC().Add(time.Minute * -20)
 	endTime := time.Now().UTC()
 	req4 := &monitoringpb.ListTimeSeriesRequest{
-		Name:   "projects/" + projectID,
-		Filter: `metric.type="` + metricName + `"`,
+		Name:   "projects/" + *projectID,
+		Filter: `metric.type="` + *metricName + `"`,
 		Interval: &monitoringpb.TimeInterval{
 			StartTime: &timestamp.Timestamp{
 				Seconds: startTime.Unix(),
@@ -106,7 +129,7 @@ func main() {
 	fmt.Println("Done")
 
 	// req3 := &monitoringpb.ListMonitoredResourceDescriptorsRequest{
-	// 	Name: "projects/" + projectID,
+	// 	Name: "projects/" + *projectID,
 	// }
 	// iter3 := c.ListMonitoredResourceDescriptors(ctx, req3)
 
